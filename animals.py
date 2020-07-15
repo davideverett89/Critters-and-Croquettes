99ebbb102d7a131f9538bfe1a44450e646678f2b
David Everett <davideverett1989@gmail.com>
2020-07-15 12:19:37 -0500

    Part 2 done.

--- animals.py
+++ animals.py
@@ -4,41 +4,46 @@ from datetime import date
 
 class Llama:
     
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
+        self.shift = shift
         self.dated_added = date.today()
         self.isWalking = True
 
 class Goat:
     
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
+        self.shift = shift
         self.dated_added = date.today()
         self.isWalking = True
 
 class Pig:
     
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
+        self.shift = shift
         self.dated_added = date.today()
         self.isWalking = True
 
 class Rabbit:
     
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
+        self.shift = shift
         self.dated_added = date.today()
         self.isWalking = True
 
 class GiantTortoise:
     
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
+        self.shift = shift
         self.dated_added = date.today()
         self.isWalking = True
 
@@ -126,11 +131,11 @@ class Frog:
         self.date_added = date.today()
         self.isSwimming = True
 
-steve = Llama('Steve', 'Llama')
-frank = Goat('Frank', 'Goat')
-bob = Pig('Bob', 'Pig')
-jeff = Rabbit('Jeff', 'Rabbit')
-george = GiantTortoise('George', 'Giant Tortoise')
+steve = Llama('Steve', 'Llama', 'morning')
+frank = Goat('Frank', 'Goat', 'evening')
+bob = Pig('Bob', 'Pig', 'midday')
+jeff = Rabbit('Jeff', 'Rabbit', 'evening')
+george = GiantTortoise('George', 'Giant Tortoise', 'midday')
 
 pete = PythonSnake('Pete', 'Python')
 rodger = CopperheadSnake('Rodger', 'Copperhead')
@@ -148,6 +153,11 @@ bertha = Frog('Bertha', 'Frog')
 
 petting_zoo = [steve, frank, bob, jeff, george, pete, rodger, maxwell, terrence, mary, reginald, katie, jessica, betty, bertha]
 
+petting_area_animals = list()
+
 for animal in petting_zoo:
-    for key, value in animal.__dict__.items():
-        print(f'{key}: {value}')
+    if hasattr(animal, 'isWalking'):
+        petting_area_animals.append(animal)
+
+for animal in petting_area_animals:
+    print(f'{animal.name} the {animal.species} is available to pet during the {animal.shift} shift.')
