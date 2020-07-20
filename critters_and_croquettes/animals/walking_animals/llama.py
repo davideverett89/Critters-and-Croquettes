72649178f62b2e739a7149d61ecf86726d58edcf
David Everett <davideverett1989@gmail.com>
2020-07-20 10:41:31 -0500

    Part 5 complete.

--- critters_and_croquettes/animals/walking_animals/llama.py
+++ critters_and_croquettes/animals/walking_animals/llama.py
@@ -2,13 +2,22 @@ from datetime import date
 
 class Llama:
     
-    def __init__(self, name, species, shift, food):
+    def __init__(self, name, chip_number, species, shift, food):
         self.name = name
+        self.__chip_number = chip_number
         self.species = species
         self.shift = shift
         self.food = food
         self.dated_added = date.today()
         self.isWalking = True
+
+    @property
+    def chip_number(self):
+        return self.__chip_number
+
+    @chip_number.setter
+    def chip_number(self, number):
+        pass
 
     def feed(self):
         print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
